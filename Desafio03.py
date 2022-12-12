84c545d3ac6e8d4fc044cdd37a081aa66dbaa6ad
Amanda Alencar <111574037+AmanditaC@users.noreply.github.com>
2022-12-12 20:20:44 +0000

    desafio 3

---
+++ Desafio03.py
@@ -0,0 +1,7 @@
+#Criando um código que leia 2 números e mostre a soma entre eles
+
+numero1 = input('Informe o primeiro numero ')
+numero2 = input('Informe o segundo número ')
+soma = int(numero1) + int(numero2)
+
+print('A soma é ', soma)
