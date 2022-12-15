74b0c9996d449c4a1affec1f79fd518ee03d4cc8
Amanda Alencar <111574037+AmanditaC@users.noreply.github.com>
2022-12-15 21:50:34 +0000

    melhorada

--- Desafio02.py
+++ Desafio02.py
@@ -1,5 +1,9 @@
-dia = input('Qual é o dia?')
-mes = input('Qual é o mês?')
-ano = input('Qual é o ano?')
+#Programa que faz a leitura de uma data
 
-print('Você nasceu no dia', dia, 'de', mes, 'de', ano, ',correto?')
+dia = int(input('Qual é o dia?'))
+mes = int(input('Qual é o mês?'))
+ano = int(input('Qual é o ano?'))
+
+#print('Você nasceu no dia', dia, 'de', mes, 'de', ano, ',correto?')
+
+print('Você nasceu no dia {} de {} de {}, correto?'.format(dia, mes, ano))
