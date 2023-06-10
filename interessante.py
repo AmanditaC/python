8f0d3a37cc76d510e12fb111e52c41cfefdce9f9
AmanditaC <amandacristinagomes8@gmail.com>
2023-06-10 15:37:05 -0300

    .

---
+++ interessante.py
@@ -0,0 +1,19 @@
+nome = input ('Qual seu nome? ')
+
+print(f'Prazer em te conhecer {nome}!\n')
+
+print(f'Prazer em te conhecer {nome:20}!\n')
+
+#Fazendo o alinhamento para a direita
+
+print(f'Prazer em te conhecer {nome:>20}!\n')
+
+#Fazendo o alinhamento para a esquerda
+
+print(f'Prazer em te conhecer {nome:<20}!\n')
+
+#Fazendo ficar centralizado
+
+print(f'Prazer em te conhecer {nome:^20}!\n')
+
+print(f'Prazer em te conhecer {nome:=^20}!\n')
