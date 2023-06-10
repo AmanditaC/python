ada373035864cb0ea9725becfda899bf707519db
AmanditaC <amandacristinagomes8@gmail.com>
2023-06-10 19:37:58 -0300

    .

--- Desafio005.py
+++ Desafio005.py
@@ -2,7 +2,11 @@
 
 n = int(input('Informe um número inteiro: '))
 
-ant = n - 1
-suc = n + 1
+#ant = n - 1
+#suc = n + 1
 
-print(f'{n} tem como sucessor {suc} e antecessor {ant}')
+#print(f'{n} tem como sucessor {suc} e antecessor {ant}')
+
+#Ou
+
+print(f'{n} tem como antecessor {n-1} e sucessor {n+1}')
