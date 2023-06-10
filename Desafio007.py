234cd2ee7b8b65d04d20474f8f4df70bc67e7873
AmanditaC <amandacristinagomes8@gmail.com>
2023-06-10 17:26:34 -0300

    Desafio 7

---
+++ Desafio007.py
@@ -0,0 +1,8 @@
+#desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+n1 = float(input('Informe a primeira nota: '))
+n2 = float(input('Informe a segunda nota: '))
+
+media = (n1 + n2)/2
+
+print(f'A média do aluno foi de {media}')
