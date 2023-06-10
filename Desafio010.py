faf9bbbb57098e8318750adb8e1a0abbeedc3c07
AmanditaC <amandacristinagomes8@gmail.com>
2023-06-10 19:21:19 -0300

    Desafio 10

--- Desafio010.py
+++ Desafio010.py
@@ -1,4 +1,8 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. 
-#Considere US$ 1.00 = R$3.27
+#Considere US$ 1.00 = R$4.88
 
-din = float
+din = float(input('Informe quanto você possui na carteira: '))
+
+conv = din / 4.88
+
+print(f'Você pode comprar até {conv: .2f} dólares')
