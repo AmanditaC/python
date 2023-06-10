5941f5da5862d9a0a4317bba8fc12087e89e84a7
AmanditaC <amandacristinagomes8@gmail.com>
2023-06-10 16:22:47 -0300

    Desafio 5

---
+++ Desafio05.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um número inteiro e mostre na tela seu sucessor e antecessor
+
+n = int(input('Informe um número inteiro: '))
+
+ant = n - 1
+suc = n + 1
+
+print(f'{n} tem como sucessor {suc} e antecessor {ant}')
