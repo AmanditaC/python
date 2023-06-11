522d637af5d769b7ab9f2f179cb667d1e1f56b2f
AmanditaC <amandacristinagomes8@gmail.com>
2023-06-11 13:26:40 -0300

    Desafio 12

--- Desafio012.py
+++ Desafio012.py
@@ -1,1 +1,9 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
+
+preço = float(input('Informe o preço do produto: '))
+
+des = (5/100) * preço
+
+Npreço = preço - des
+
+print(f'\nCom um desconto de 5% seu produto ficar por {Npreço}')
